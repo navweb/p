91c21b8b78e76b734266f5f3ed565cbfccf5be50
navweb <113300523+navweb@users.noreply.github.com>
2022-09-25 19:54:48 +0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,28 @@
+             <!-- Область основного контента -->
+            
+                <form action="./mail/totelegram.php" method="post">
+
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Имя</label>
+                  <input class="form-control contact__form-input" name="title" type="text" placeholder="Имя"><br>
+                </div>  
+                
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Номер</label>
+                  <input class="form-control contact__form-input" name="test" type="text" placeholder="Номер"><br>
+                </div> 
+
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Почта</label>
+                  <input class="form-control contact__form-input" name="email" type="text" placeholder="Почта"><br>
+                </div> 
+
+                <div class="contact__form-div contact__form-area">
+                            <label for="" class="contact__form-tag">Проект</label>
+                            <textarea name="content" id="" cols="30" rows="10" placeholder="Сообщение"
+                             class="contact__form-input"></textarea>
+                        </div>
+                  <input type="submit" value="Отправить" class="btn btn-primary button">
+                </form>
+             
+            <!-- Область основного контента --> 
---
+++ contactEn.php
@@ -0,0 +1,28 @@
+             <!-- Область основного контента -->
+            
+                <form action="./mail/totelegram.php" method="post">
+
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Name</label>
+                  <input class="form-control contact__form-input" name="title" type="text" placeholder="Your name"><br>
+                </div>  
+                
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Number</label>
+                  <input class="form-control contact__form-input" name="test" type="text" placeholder="Tel Number"><br>
+                </div> 
+
+                <div class="contact__form-div">
+                  <label for="" class="contact__form-tag">Email</label>
+                  <input class="form-control contact__form-input" name="email" type="text" placeholder="Your Email"><br>
+                </div> 
+
+                <div class="contact__form-div contact__form-area">
+                            <label for="" class="contact__form-tag">Project</label>
+                            <textarea name="content" id="" cols="30" rows="10" placeholder="Message(not necessary)"
+                             class="contact__form-input"></textarea>
+                        </div>
+                  <input type="submit" value="Send" class="btn btn-primary button">
+                </form>
+             
+            <!-- Область основного контента --> 
